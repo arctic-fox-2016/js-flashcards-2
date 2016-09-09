70f7a0fec809b098a84a8d6832f3058b50dc3b9a
sahbanagold <sahbanao@gmail.com>
2016-09-09 17:48:22 +0700

    flashcard 2 ok

--- controller.js
+++ controller.js
@@ -1,2 +1,39 @@
 "use strict"
+import View from './view.js'
 // write your code here
+class Controller {
+
+   static set_file(fs, deck) {
+      Controller.soal = JSON.parse(fs.readFileSync(deck))
+      Controller.score = 100
+      Controller.answer_true = 0
+      Controller.answer_wrong = 0
+
+   }
+
+   static play(rl) {
+      Controller.get_answer(rl, Controller.soal, 0)
+   }
+
+   static get_answer(rl, question, idx) {
+      if (idx < question.length) {
+         rl.question(question[idx].definition + " :", (user_ans) => {
+            if (question[idx].term.toLowerCase() == user_ans.toLowerCase()) {
+               console.log("thats it!!")
+               Controller.answer_true++
+                  View.print_current_score(Controller.score, Controller.answer_true, Controller.answer_wrong)
+               Controller.get_answer(rl, question, idx + 1)
+            } else {
+               console.log("It's not that hard man!!");
+               Controller.answer_wrong++
+                  Controller.score -= 10
+               Controller.get_answer(rl, question, idx)
+            }
+         })
+      } else {
+         View.print_final_score(Controller.score)
+         rl.close()
+      }
+   }
+}
+export default Controller
--- flashcards.js
+++ flashcards.js
@@ -1,2 +1,41 @@
 "use strict"
-// write your code here
+import Controller from './controller.js'
+import View from './view.js'
+const fs = require('fs')
+const readline = require('readline')
+const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+   })
+   // write your code here
+class Flashcards {
+   constructor() {}
+
+   init() {
+      let deck = process.argv[2]
+      Flashcards.rlo = rl
+      View.print_help()
+      if (deck) {
+         console.log("anda pilih deck sendiri")
+         Controller.set_file(fs, deck)
+         Controller.play(rl)
+      } else {
+         this.default()
+      }
+   }
+
+   default () {
+      rl.question('mau main dengan deck default ?', (ans) => {
+         if (ans === "y") {
+            console.log('ok kita main ')
+            Controller.set_file(fs, "social.json")
+            Controller.play(rl)
+         } else {
+            rl.close()
+         }
+      })
+
+   }
+}
+let fcard = new Flashcards()
+fcard.init()
