9f06220681e42dca37240b921f6dbe6124840c20
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-09-09 16:08:36 +0700

    add view and deck for flashcard

--- view.js
+++ view.js
@@ -1,2 +1,21 @@
 "use strict"
 // write your code here
+class View {
+  static print_help() {
+    console.log(`==============> Welcome to Sahbana Flashcard <==============`);
+    console.log(`Type 'node flashcard.js social' for social-type flashcard `);
+    console.log(`Type 'node flashcard.js science' for science-type flashcard`);
+  }
+
+  static print_final_score(final_score) {
+    console.log(`Final Score     : ${final_score}`);
+    console.log(`Thank you for playing !`);
+  }
+
+  static print_current_score(current_score, answer_true, answer_wrong) {
+    console.log(`Current Score   : ${current_score}`);
+    console.log(`Answer true     : ${answer_true}`);
+    console.log(`Answer wrong    : ${answer_wrong}`);
+
+  }
+}
