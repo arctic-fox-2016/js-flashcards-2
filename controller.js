4910df73a0bfdefea52be38362825425ec0fe154
Imam <email": null>
2016-09-11 12:10:28 +0700

    imam

--- controller.js
+++ controller.js
@@ -1,2 +1,30 @@
 "use strict"
 // write your code here
+import View from './view.js'
+class Controller {
+
+  static questions(fs){
+     Controller.ques = JSON.parse(fs.readFileSync("social.json"))
+  }
+
+  static playGame(rl){
+      Controller.flash(rl, Controller.ques,0)
+  }
+
+static flash(rl, flashcard,index){
+  if(index < flashcard.length){
+  rl.question(flashcard[index].definition,(guess)=>{
+    if(flashcard[index].term.toLowerCase() == guess.toLowerCase()) {
+      View.correct(index)
+      Controller.flash(rl, flashcard,index+1)
+      } else{
+      View.wrong(index)
+      Controller.flash(rl, flashcard,index)
+    }
+  })
+  } else{
+    rl.close()
+  }
+}
+}
+export default Controller
--- flashcards.js
+++ flashcards.js
@@ -1,2 +1,29 @@
-"use strict"
+'use strict'
 // write your code here
+import View from './view.js'
+import Controller from './controller.js'
+const fs = require ('fs')
+const readline = require ('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+class Flashcards {
+
+  main () {
+    rl.question('Tekan enter untuk mulai main ?\n',(ans) => {
+       if (ans === "") {
+           Controller.questions(fs, "social.json")
+           Controller.playGame(rl)
+        } else {
+           rl.close()
+        }
+      })
+
+      }
+  }
+
+View.title()
+let play = new Flashcards()
+play.main()
